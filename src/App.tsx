f644d4e97e7d56e1f846b07c546dc92fdcd221cf
Luiz Spinelli <luiz.luchi@gmail.com>
2021-03-27 19:01:01 -0300

    global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 export function App() {
   return (
-    <div>Hello World</div>
+    <h1>Hello World</h1>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import { GlobalStyle } from './styles/globas';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <>
+      <App />
+      <GlobalStyle />
+    </>
   </React.StrictMode>,
   document.getElementById('root')
 );
