d33ff554dab5de5b59f94fca3979e1edf433093d
Luiz Spinelli <luiz.luchi@gmail.com>
2021-03-28 18:12:56 -0300

    newtransactionmodal

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOPen, setIsNewTransactionModal] = useState(false);
+
+  function handleOpenNewTransactionModal () {
+    setIsNewTransactionModal(true);
+  }
+
+  function handleCloseNewTransactionModal () {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
+      <NewTransactionModal isOpen={isNewTransactionModalOPen} onRequestClose={handleCloseNewTransactionModal} />
     </>
   );
 }
